836fa5d36c034315ba25fddba7109d0e98f8a1ab
apolgar <akos.polgar@parkbob.com>
2018-03-25 09:06:12 +0200

    First working version -> 1.0

--- redShifter.sh
+++ redShifter.sh
@@ -2,25 +2,25 @@
 
 case $1 in
      "Budapest")      
-          redshift -l 47.49801:19.03991 &
+          redshift-gtk -l 47.49801:19.03991 &
           ;;
      "Papa")      
-          redshift -l 47.33004:17.4674 &
+          redshift-gtk -l 47.33004:17.4674 &
           ;;
      "Pecs")
-          redshift -l 46.08333:18.23333 &
+          redshift-gtk -l 46.08333:18.23333 &
           ;; 
      "Augsburg")
-          redshift -l 48.37154:10.89851 &
+          redshift-gtk -l 48.37154:10.89851 &
           ;;
      "Munich")
-          redshift -l 48.13743:11.57549 &
+          redshift-gtk -l 48.13743:11.57549 &
           ;;
      "Vienna")
-          redshift -l 48.20849:16.37208 &
+          redshift-gtk -l 48.20849:16.37208 &
           ;;
      "Canillas")
-          redshift -l 36.8734800:-4.0813300 &
+          redshift-gtk -l 36.8734800:-4.0813300 &
           ;;
      *)
           echo Missing or invalid city name
